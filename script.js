dffab5ac4577246ea10218f35cf5089af1e9c9ff
Arbab Ahamad <ahmadarbab.456@gmail.com>
2026-05-21 23:42:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function getFormvalue() {
-    //Write your code here
 
+	// prevent form from refreshing the page
+	event.preventDefault();
+
+	// get input values and remove extra space
+	let firstName = document.querySelector('input[name="fname"]').value.trim();
+	let lastName = document.querySelector('input[name="lname"]').value.trim();
+
+	// display full name
+	alert(firstName + " " + lastName);
 }
